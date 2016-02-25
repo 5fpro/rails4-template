d4e740767b320afc2375bb6bc7afbc84a3cfa417
Dylan Lin <dylanmail0203@gmail.com>
2016-02-25 15:29:00 +0800

    Modify spec/controllers/sites_conteoller_spec.rb to pass new routing.
    Modify spec/controllers/base_conteoller_spec.rb to pass new routing.
    
    Modify spec/controllers/authorizations_conteoller_spec.rb to pass new routing.

--- spec/controllers/sites_controller_spec.rb
+++ spec/controllers/sites_controller_spec.rb
@@ -14,15 +14,42 @@
 require 'rails_helper'
 
 RSpec.describe SitesController, type: :request do
-  let(:site) { FactoryGirl.create :site }
+  let(:site) { FactoryGirl.create :site, host:"example.com" }
 
   describe "#show" do
-    before { get "/sites/#{site.id}" }
+    before { get "http://#{Setting.host}/sites/#{site.id}" }
     it { expect(response).to be_success }
+  end
+
+  describe "#show via subdomain" do
+    before { get "http://#{site.subdomain}.#{Setting.host}" }
+    it { expect(response).to be_success }
+  end
+
+  describe "#show via custom domain" do
+    it "assigns site" do
+      host!(site.host)
+    	get "/"
+    	expect(assigns(:site).id).to eq(site.id)
+   	end
   end
 
   describe "#edit" do
-    before { get "/sites/#{site.id}/edit" }
+    before { get "http://#{Setting.host}/sites/#{site.id}/edit" }
     it { expect(response).to be_success }
   end
+
+  describe "#edit via subdomain" do
+    before { get "http://#{site.subdomain}.#{Setting.host}/edit" }
+    it { expect(response).to be_success }
+  end
+
+  describe "#edit via custom domain" do
+    it "assigns site" do
+      host!(site.host)
+    	get "/edit"
+    	expect(assigns(:site).id).to eq(site.id)
+   	end
+  end
+
 end
