d832b75aeb5ab5c6e4f3d6e7f3a7811d178abad7
yichaulin <yichaulin@gmail.com>
2016-08-06 16:40:36 +0800

    add batch_delete api for Site

--- app/controllers/api/sites_controller.rb
+++ app/controllers/api/sites_controller.rb
@@ -4,5 +4,15 @@ module Api
     def index
       @sites = Site.all
     end
+
+    def batch_delete
+
+      params[:ids].each do |id|
+        site = Site.find_by(:id => id)
+        site.destroy
+      end
+
+      render :json => nil, :status => 200
+    end
   end
 end
--- spec/controllers/api/sites_controller_spec.rb
+++ spec/controllers/api/sites_controller_spec.rb
@@ -16,4 +16,24 @@ RSpec.describe Api::SitesController, type: :request do
       expect(response.status).to eq(200)
     end
   end
+
+  describe "#batch_delete" do
+
+    before do
+      FactoryGirl.create(:site)
+      FactoryGirl.create(:site)
+
+    end
+
+    it "should delete all datas" do
+
+      expect(Site.all.size).to be(2)
+      delete_list = Site.all.map{|v| v.id }
+
+      post "/api/sites/batch_delete", :ids => delete_list
+
+      expect(Site.all.size).to be(0)
+    end
+
+  end
 end
