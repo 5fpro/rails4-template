fd8c8e397c957f3b99876a90cd0ccea3b86140c7
Tim Wu <tim7012@msn.com>
2016-08-06 15:46:43 +0800

    site/page CRUD

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -13,15 +13,54 @@
 
 class SitesController < ApplicationController
   before_action :find_site
+
+  def index
+    @sites = Site.all("updated_at desc")
+  end
+
   def show
+    @pages = @site.pages.order("updated_at desc")
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @site = Site.new(site_params)
+    if @site.save
+      flash[:notice] = "site was successfully created"
+      redirect_to :index
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  private
+  def update
+    if @site.update(site_params)
+      flash[:notice] = "update successfully"
+      redirect_to :index
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @site.destroy
+    flash[:alert] = "The site was successfully deleted"
+    redirect_to :index
+  end
+
+  protected
 
   def find_site
     @site = Site.find(params[:id])
+  end
+
+  def site_params
+    params.require(:site).permit(:name, :host, :subdomain, data: Site.stored_attributes[:data])
   end
 end
