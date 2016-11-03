e4725579a930bc4756d572bf61bf8b0203c3a989
A-hu <louis0911317399@gmail.com>
2016-11-03 15:58:46 +0800

    api sites controller

--- app/controllers/api/sites_controller.rb
+++ app/controllers/api/sites_controller.rb
@@ -4,5 +4,20 @@ module Api
     def index
       @sites = Site.all
     end
+
+    def create
+    	@site = Site.new( name: params[:name], host: params[:host], subdomain: params[:subdomain],data: params[:data] )
+        @site.save
+    end
+
+    def update
+    	@site = Site.find(params[:id])
+    	@site = Site.update( name: params[:name], host: params[:host], subdomain: params[:subdomain],data: params[:data] )
+    end
+
+    def delete
+    	@site = Site.find(params[:id])
+    	@site.destroy
+    end
   end
 end
