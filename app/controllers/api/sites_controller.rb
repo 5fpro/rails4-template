37670cccbb94fb745382464610a0881c495c4065
jacobshan <jacobshan429@gmail.com>
2016-11-26 00:29:31 +0800

    finished site api update and destroy

--- app/controllers/api/sites_controller.rb
+++ app/controllers/api/sites_controller.rb
@@ -10,8 +10,23 @@ module Api
       if @site.save
         render :json => @site.to_json, :status => 200
       else
-        render :status => 400, :json => @site.errors.to_json
+        render :status => 400, :json => { :error => @site.errors.full_messages.join(",") }
       end
+    end
+
+    def update
+      @site = Site.find(params[:id])
+      if @site.update(params_site)
+        render :json => @site, :status => 200
+      else
+        render :status => 400, :json => { :error => @site.errors.full_messages.join(",") }
+      end
+    end
+
+    def destroy
+      @site = Site.find(params[:id])
+      @site.destroy
+      render :json => { :message => "site deleted!"}, :status => 200
     end
 
     private
--- spec/controllers/api/sites_controller_spec.rb
+++ spec/controllers/api/sites_controller_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe Api::SitesController, type: :request do
   end
 
   describe "#create" do
+
     it "should return 200 when successfully created" do
       post "/api/sites", :site => { :name => 'aaa', :host => 'bbb', :subdomain => 'ccc', :data => 'ddd' }
       expect(response.status).to eq(200)
@@ -26,6 +27,39 @@ RSpec.describe Api::SitesController, type: :request do
     it "should return 400 when failed to create" do
       post "/api/sites", :site => { :name => '' }
       expect(response.status).to eq(400)
+    end
+
+  end
+
+  describe "#update" do
+
+    before do
+      Site.create( :id => 1, :name => "aaa", :host => "bbb", :subdomain => "ccc", :data => "ddd")
+    end
+
+    it "should update site name" do
+      patch "/api/sites/1", :site => { :name => "updated" }
+      site = Site.find(1)
+      expect(site.name).to eq("updated")
+    end
+
+    it "should return 400 when failed to update" do
+      patch "/api/sites/1", :site => { :name => "" }
+      site = Site.find(1)
+      expect(response.status).to eq(400)
+    end
+
+  end
+
+  describe "#destroy" do
+
+    before do
+      Site.create( :id => 1, :name => "aaa", :host => "bbb", :subdomain => "ccc", :data => "ddd")
+    end
+
+    it "should return 200 when successfully deleted" do
+      delete "/api/sites/1"
+      expect(response.status).to eq(200)
     end
   end
 end
