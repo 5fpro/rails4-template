16a53e8d99f38f8a25d12b2d58a3e03506355e44
HEROGWP <pp820819@gmail.com>
2016-11-29 13:31:08 +0800

    Q-3 can use ajax create / update / destroy

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,13 @@ module Myapp
 
     config.i18n.default_locale = "en"
 
+    config.middleware.insert_before 0, 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
