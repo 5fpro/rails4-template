7ac01a852ad13a662619d1bc00d3aea88e51f4f3
HEROGWP <pp820819@gmail.com>
2016-12-06 19:06:35 +0800

    sites CRUD

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -12,16 +12,63 @@
 #
 
 class SitesController < ApplicationController
-  before_action :find_site
+  before_action :find_site, only: [:show, :edit, :update, :destroy]
+  
+  def index
+  	@sites = Site.all
+  end
+  
   def show
+  	
+  end
+
+  def new
+  	@site = Site.new
+  end
+
+  def create 
+  	@site = Site.new(site_params)
+
+  	if @site.save
+  		flash[:notice] = "success to create"
+      redirect_to site_path(@site)
+  	else
+  		flash[:alert] = "failed to create"
+      render :new
+  	end
   end
 
   def edit
+  	
   end
+
+  def update
+  	
+  	if @site.update(site_params)
+  	 	flash[:notice] = "success to update"
+      redirect_to site_path(@site)
+    else
+  	 	flash[:alert] = "failed to update"
+      render :edit
+    end
+
+  end
+
+  def destroy
+  	@site.destroy
+  	flash[:notice] = "success to delete"
+  	redirect_to sites_path
+  end
+
 
   private
 
   def find_site
     @site = Site.find(params[:id])
   end
+
+  def site_params
+		params.require(:site).permit(:name, :host, :data)
+	end
+
 end
