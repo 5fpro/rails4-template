8273c9ad18be76c09c9ff36bc3ba9274f702c5b6
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2016-12-09 13:27:08 +0800

    Rails Q3 enable CORS

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 gem 'rails', '4.2.6'
+# API
+gem 'rack-cors', require: 'rack/cors'
 
 # DB
 gem 'pg'
--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,6 +2,7 @@ module Api
   class BaseController < ApplicationController
   	skip_before_action :verify_authenticity_token
   	before_action :set_locale
+    before_action :enable_cors
     def index
       @message = "api root"
     end
@@ -13,6 +14,13 @@ module Api
     private
     def accept_language_header
     	request.env['HTTP_ACCEPT_LANGUAGE'].split(',').first rescue I18n.default_locale
+    end
+    def enable_cors
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Atmosphere-tracking-id, X-Atmosphere-Framework, X-Cache-Date, Content-Type, X-Atmosphere-Transport, X-Remote, api_key, auth_token, *'
+      response.headers['Access-Control-Request-Method'] = 'GET, POST, PUT, DELETE'
+      response.headers['Access-Control-Request-Headers'] = 'Origin, X-Atmosphere-tracking-id, X-Atmosphere-Framework, X-Cache-Date, Content-Type, X-Atmosphere-Transport,  X-Remote, api_key, *'
     end
   end
 end
